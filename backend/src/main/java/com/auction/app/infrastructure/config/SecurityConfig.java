d97a3805ca6c98e18d5dd110842656d332129467
LKLD1909 <codeboy1909@gmail.com>
2026-06-09 11:09:08 +0700

    Fix Docker thing

--- backend/src/main/java/com/auction/app/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/com/auction/app/infrastructure/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
+                "http://localhost:80",
                 "http://localhost:5173",
                 "http://localhost:8000",
                 "http://127.0.0.1:8000",
